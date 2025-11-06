be33e68943055928ef3b26d48f836313da17fff2
Ivan <vanyagorin@gmail.com>
2025-11-06 13:23:32 +0100

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          black: "#000000",
+          green: "#22c55e", // = tailwind green-500
+          yellow: "#eab308" // = tailwind yellow-500
+        },
+      },
+      borderRadius: {
+        '2xl': '1rem',
+      },
+      boxShadow: {
+        soft: "0 10px 30px rgba(0,0,0,.35)",
+      },
+    },
   },
   plugins: [],
 }
